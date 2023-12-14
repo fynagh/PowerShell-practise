763d0f151be5f95d0be165bcb05aa1ce5d8f39c9
Fidan Naghiyeva <152993580+fynagh@users.noreply.github.com>
2023-12-14 17:14:07 +0400

    Update tasks.ps1

--- tasks.ps1
+++ tasks.ps1
@@ -1,39 +1,13 @@
-$age = 23
-$element1 = 52 + $age
-$element2 = 49 - $age
-$dataset = $element1, $element2
-$dataset += $age
-$second = -join $dataset
-$ts = [timespan]::fromseconds($second)
-(“{0:dd\:hh\:mm\:ss}” -f $ts)
-
-
-$my_name = “Fidan”
-$sum_my_name = ($my_name.ToUpper()[0..2] | ForEach-Object { [int][char]$_ -
-[int][char]&#39;A&#39; + 1 }) | Measure-Object -Sum | Select-Object -ExpandProperty Sum
-$sum_my_name
-$friend1 = &quot;Amina&quot;
-$sum_friend1 = ($friend1.ToUpper()[-3..-1] | ForEach-Object { [int][char]$_ -
-[int][char]&#39;A&#39; + 1 }) | Measure-Object -Sum | Select-Object -ExpandProperty Sum
-$friend2 = &quot;Anar&quot;
-$sum_friend2 = ($friend2.ToUpper()[-3..-1] | ForEach-Object { [int][char]$_ -
-[int][char]&#39;A&#39; + 1 }) | Measure-Object -Sum | Select-Object -ExpandProperty Sum
-
-$friend3 = &quot;Zumrud&quot;
-$sum_friend3 = ($friend3.ToUpper()[-3..-1] | ForEach-Object { [int][char]$_ -
-[int][char]&#39;A&#39; + 1 }) | Measure-Object -Sum | Select-Object -ExpandProperty Sum
-$dataset = $sum_my_name, $sum_friend1, $sum_friend2, $sum_friend3
-($dataset[0] -lt $dataset[2]) -or ! ($dataset[1] -gt $dataset[3])
-
-
-
 function division{
 $number = read-host "Enter a number"
+[int]$prime = 0
 for($digit = 2; $digit -le 9; $digit++){
- if ($number % $digit -eq 0) {
-       Write-Host "$number is divisible by $digit"
-    } 
-  }
+if ($number % $digit -eq 0) {
+Write-Host "$number is divisible by $digit"
+$prime = 1}                                
+}           
+if ($prime -eq 0){
+write-host "It is prime number"}
 }
 
 
@@ -42,14 +16,20 @@ function MasterFunction {
    [int]$num = read-host ”ENTER NUMBER”
 
     function CalculateSquare {
-        return $num * $num
+        $sqr = $num*$num
+        return "The square of a number is $sqr"
+    }
+
+    function CalculateSquareRoot {
+         $sqrt = [Math]::Sqrt($num)
+        return "The square root of a number is $sqrt"
     }
 
     function CheckEvenOdd {
         if ($num % 2 -eq 0) {
-            return "Even"
+            return "It is even"
         } else {
-            return "Odd"
+            return "It is odd"
         }
     }
 
@@ -63,10 +43,9 @@ function MasterFunction {
     }
 
  CalculateSquare
+ CalculateSquareRoot
  CheckEvenOdd
  CategorizeNumber
 }
-
-
 
 
